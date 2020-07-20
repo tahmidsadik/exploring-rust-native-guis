009b676a316a08d5a892ef364bf0cdb6dd5d2b2e
Tahmid Sadik <tahmidsadik112@gmail.com>
2020-07-20 20:38:34 +0600

    Initial Commit

---
+++ src/main.rs
@@ -0,0 +1,116 @@
+use iced::{
+    button, text_input, Align, Application, Button, Column, Command, Container, Element, Font,
+    HorizontalAlignment, Length, Row, Settings, Text, TextInput,
+};
+
+const DEFAULT_FONT: Font = Font::External {
+    name: "DefaultFont",
+    bytes: include_bytes!("resources/Cascadia.ttf"),
+};
+
+const MAX_WIDTH: u16 = 500;
+
+pub fn main() {
+    Counter::run(Settings::default())
+}
+
+#[derive(Default)]
+struct Counter {
+    input_val: String,
+    input_state: text_input::State,
+    task_list: Vec<Task>,
+    latest_task_id: i32,
+}
+
+struct Task {
+    id: i32,
+    description: String,
+    is_complete: bool,
+    mark_as_complete_button_state: button::State,
+}
+
+impl Task {
+    fn new(id: i32, description: String) -> Self {
+        Task {
+            id,
+            description,
+            is_complete: false,
+            mark_as_complete_button_state: button::State::new(),
+        }
+    }
+}
+
+#[derive(Debug, Clone)]
+enum Message {
+    TextInputChanged(String),
+    AddNewTask,
+}
+
+impl Application for Counter {
+    type Message = Message;
+
+    fn new() -> (Self, Command<Message>) {
+        (Self::default(), Command::none())
+    }
+
+    fn title(&self) -> String {
+        String::from("A simple counter")
+    }
+
+    fn update(&mut self, message: Message) -> Command<Message> {
+        match message {
+            Message::TextInputChanged(s) => {
+                self.input_val = s;
+            }
+            Message::AddNewTask => {
+                if !self.input_val.is_empty() {
+                    self.task_list
+                        .push(Task::new(self.latest_task_id + 1, self.input_val.clone()));
+                    self.latest_task_id += 1;
+                    self.input_val.clear();
+                }
+            }
+        }
+
+        Command::none()
+    }
+
+    fn view(&mut self) -> Element<Message> {
+        let tasks = self
+            .task_list
+            .iter_mut()
+            .fold(Column::new().spacing(5), |col, task| {
+                col.push(
+                    Row::new().push(
+                        Text::new(format!("• {}. {}", task.id, task.description))
+                            .size(18)
+                            .font(DEFAULT_FONT),
+                    ),
+                )
+            });
+
+        let content = Column::new()
+            .max_width(MAX_WIDTH as u32)
+            .spacing(15)
+            .padding(40)
+            .width(Length::Fill)
+            .push(Text::new("master of the todos").size(42).font(DEFAULT_FONT))
+            .push(
+                TextInput::new(
+                    &mut self.input_state,
+                    "Placeholder for text input...",
+                    &self.input_val,
+                    Message::TextInputChanged,
+                )
+                .width(Length::Units(MAX_WIDTH))
+                .padding(10)
+                .on_submit(Message::AddNewTask),
+            )
+            .push(tasks);
+        Container::new(content)
+            .width(Length::Fill)
+            .center_x()
+            .center_y()
+            .into()
+    }
+}
